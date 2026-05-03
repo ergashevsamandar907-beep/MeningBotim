67855c27d3e2ae6dfb8c6fa2fb1913005acc3ffb
ergashevsamandar907-beep <ergashevsamandar907@gmail.com>
2026-05-03 19:18:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import os
+from aiohttp import web
 import asyncio
 import sqlite3
 import random
@@ -499,5 +501,23 @@ async def main():
     print("---------------------------------------")
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def handle(request):
+    return web.Response(text="Bot is running!")
+
+app = web.Application()
+app.router.add_get("/", handle)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    
+    runner = web.AppRunner(app)
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(runner.setup())
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    loop.run_until_complete(site.start())
+    
+    print("Bot ishga tushdi...")
+    # DIQQAT: O'chirib tashlagan kodingiz ichidagi botni ishga tushiruvchi 
+    # oxirgi qatorni aynan shu yerga yozasiz. Masalan:
+    dp.run_polling(bot)
